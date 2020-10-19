d3a91ac528e12dbfd983cd53963b6524d735b2d8
rhw <ruohanw89@gmail.com>
2020-10-19 00:16:16 -0700

    connect to mongoDb backend throught apollo-server provider

---
+++ client/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_THOUGHTS } from '../utils/queries';
+const Home = () => {
+  // use useQuery hook to make query request
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+  // access to thought data
+  const thoughts = data?.thoughts || [];
+  console.log(thoughts);
+
+  return (
+    <main>
+      <div className="flex-row justify-space-between">
+        <div className="col-12 mb-3">{/* PRINT THOUGHT LIST */}</div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
