710fd7e66c51f722e372b31110f4d19e10ba2738
rhw <ruohanw89@gmail.com>
2020-10-19 02:49:18 -0700

    set up main url

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,14 @@ import React from 'react';
 // add these two library import statements
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
+// import BrowerRouter as Router
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import pages
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 // import components
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -15,13 +23,22 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile" component={Profile} />
+              <Route exact path="/thought" component={SingleThought} />
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
